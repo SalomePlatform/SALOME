cb102058652f22df9746ad786d5b082cdfedb33b
CEA Support SALOME <support-salome@cea.fr>
2015-06-29 16:05:53 +0200

    update according recent changes in CMake macros

--- src/resources/LightApp_msg_en.ts
+++ src/resources/LightApp_msg_en.ts
@@ -17,7 +17,7 @@
     </message>
     <message>
         <source>APP_VERSION</source>
-        <translation>V7.5.1</translation>
+        <translation>Vmaster</translation>
     </message>
     <message>
         <source>ABOUT_LICENSE</source>
@@ -25,7 +25,7 @@
     </message>
     <message>
         <source>ABOUT_VERSION</source>
-        <translation>Version 7.5.1</translation>
+        <translation>Version master</translation>
     </message>
     <message>
         <source>ABOUT_TITLE</source>
