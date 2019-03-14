79f79e3d68a2e1f4e196c80b066057f8a0a1fa9f
Nabil Ghodbane <nabil.ghodbane@cea.fr>
2019-03-14 15:33:06 +0100

    SHAPER #16685 Bug titre fenêtre Salome

--- resources/LightApp_msg_fr.ts
+++ resources/LightApp_msg_fr.ts
@@ -17,7 +17,7 @@
     </message>
     <message>
         <source>APP_VERSION</source>
-        <translation>9.2.0</translation>
+        <translation>9.3.0</translation>
     </message>
     <message>
         <source>ABOUT_LICENSE</source>
@@ -25,7 +25,7 @@
     </message>
     <message>
         <source>ABOUT_VERSION</source>
-        <translation>Version 9.2.0</translation>
+        <translation>Version 9.3.0</translation>
     </message>
     <message>
         <source>ABOUT_TITLE</source>
